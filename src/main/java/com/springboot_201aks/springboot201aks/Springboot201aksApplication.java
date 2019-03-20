2e89fa854d760734ef48ea0ec844c417c25d8741
asarkar <asarkar@promnetwork.com>
2019-03-20 14:42:48 -0400

    Initial commit

---
+++ src/main/java/com/springboot_201aks/springboot201aks/Springboot201aksApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_201aks.springboot201aks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot201aksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot201aksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_201aks/springboot201aks/Springboot201aksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot_201aks.springboot201aks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot201aksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
